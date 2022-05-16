47545794533fe659d15a6e6f8cb427f40cceec7c
rossoshanskiy98 <103197594+rossoshanskiy98@users.noreply.github.com>
2022-05-16 20:56:49 +0400

    Add files via upload
    Внедрил entityManager в класс UserDaoImp при помощи аннотации @PersistenceContext

--- src/main/java/hiber/config/JavaConfig.java
+++ src/main/java/hiber/config/JavaConfig.java
@@ -12,6 +12,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -3,39 +3,29 @@ package hiber.dao;
 import hiber.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.*;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-    @Autowired
-    EntityManagerFactory entityManagerFactory;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Override
     public void add(User user) {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
         entityManager.persist(user);
-        entityManager.getTransaction().commit();
-        entityManager.close();
     }
 
     @Override
     public void delete(User user) {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
         entityManager.remove(entityManager.find(User.class, user.getId()));
-        entityManager.getTransaction().commit();
-        entityManager.close();
     }
 
     @Override
     public void update(long id, User user) {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
         Query query = entityManager.
                 createNativeQuery("UPDATE users SET firstName = :firstName, lastName = :lastName, age = :age WHERE id = :id");
         query.setParameter("firstName", user.getFirstName());
@@ -43,19 +33,17 @@ public class UserDaoImp implements UserDao {
         query.setParameter("age", user.getAge());
         query.setParameter("id", id);
         query.executeUpdate();
-        entityManager.getTransaction().commit();
-        entityManager.close();
     }
 
     @Override
     public List<User> listUsers() {
-        TypedQuery<User> query = entityManagerFactory.createEntityManager().createQuery("SELECT u FROM User u", User.class);
+        TypedQuery<User> query = entityManager.createQuery("SELECT u FROM User u", User.class);
         return query.getResultList();
     }
 
     @Override
     public User getUserById(long id) {
-         TypedQuery<User> query = entityManagerFactory.createEntityManager().createQuery("SELECT u FROM User u WHERE u.id = :id", User.class);
+         TypedQuery<User> query = entityManager.createQuery("SELECT u FROM User u WHERE u.id = :id", User.class);
          query.setParameter("id", id);
          return query.getSingleResult();
     }
